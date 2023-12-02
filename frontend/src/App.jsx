e6674c2aecfe29b2b4dd6d092d5023b224f29263
Alok Singh <aloksingh@Aloks-MacBook-Air.local>
2023-12-02 14:08:11 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+
+import  { styled } from "styled-components"
+
+const Container = styled.div`
+  display: flex;
+`
+const Header = styled.div`
+  color: red;
+  background-color: black;
+`
+
+
+function App() {
+
+  return (
+  <Container>
+  <Header> login </Header>
+
+    hellow world
+  </Container>
+  )
+}
+
+export default App
